e277b8424bf83918696b7d273e55843865e38971
Rohan <rohan.dalvi@me.com>
2013-11-19 14:46:09 -0500

    Connection to rally added

---
+++ connect.rb
@@ -0,0 +1,18 @@
+require 'rally_api_emc_sso'
+#Setting custom headers
+headers = RallyAPI::CustomHttpHeader.new()
+headers.name = "My Utility"
+headers.vendor = "MyCompany"
+headers.version = "1.0"
+
+#or one line custom header
+
+workspace = ""
+project = ""
+
+config = {:base_url => "https://rally1.rallydev.com/slm"}
+config[:workspace]  = "#{workspace}"
+config[:project]    = "#{project}"
+config[:headers]    = headers #from RallyAPI::CustomHttpHeader.new()
+
+@rally = RallyAPI::RallyRestJson.new(config)
